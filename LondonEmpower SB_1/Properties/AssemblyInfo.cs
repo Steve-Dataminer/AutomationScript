81c479b56354e27801b766eabe99322929a4a25d
stb85 <stb85@UK004201782457>
2023-06-13 14:20:45 +0100

    Add project files.

---
+++ LondonEmpower SB_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LondonEmpower SB_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("LondonEmpower SB_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("CF0A90B3-CD26-4F7B-8B91-81742A09FCCF")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,1 @@
+# LondonEmpower SB
